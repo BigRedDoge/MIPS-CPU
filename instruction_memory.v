07da093f8cc2ca5d1309ca8af1885bd9cbfa635e
SCliffordGreybox <seancliff01@gmail.com>
2025-02-25 10:05:45 -0500

    boilerplate for instruction memory module

--- instruction_memory.v
+++ instruction_memory.v
@@ -1,6 +1,6 @@
 /*
-Instruction memory module.
-Word size: 16 bits, word addressed, size: 1024 bytes. Implemented by reg array in Verilog.
+Instruction memory module
+Word size: 16 bits, word addressed, size: 1024 bytes. Implemented by reg array in Verilog
 */
 module instruction_memory()
 
